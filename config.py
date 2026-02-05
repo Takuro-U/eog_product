8e49772ae96185b9649df9a1805406fc677c1ae7
Takuro-U <ta.kurou.1100ism@gmail.com>
2026-02-05 21:10:39 +0900

    Remove data directory from tracking

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # シリアル通信設定
-# PORT = "/dev/cu.usbmodem103"
-PORT = "/dev/cu.usbmodemF0F5BD5281842"
+PORT = "/dev/cu.usbmodem1103"
+# PORT = "/dev/cu.usbmodemF0F5BD5281842"
 BAUDRATE = 115200
 
 # ADC設定
